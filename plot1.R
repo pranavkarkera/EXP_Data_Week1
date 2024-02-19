72e8c691bcba89de12e3070ef8fda40303d36b2e
Pranav Karkera <157125124+pranavkarkera@users.noreply.github.com>
2024-02-19 20:00:01 +0530

    Delete plot1.R

--- plot1.R
+++
@@ -1,9 +0,0 @@
-file <- "./household_power_consumption.txt"
-data <- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE)
-subsetdata <- subset(data, Date == "2007-02-01" | Date =="2007-02-02")
-
-globalActivePower <- as.numeric(subsetData$Global_active_power)
-png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-dev.off()
-
